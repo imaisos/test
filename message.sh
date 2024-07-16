d7679fca1654b2fe9f3d5a473e6ef7ac3b5a9304
imaisos <78826280+imaisos@users.noreply.github.com>
2024-07-16 17:26:29 +0200

    Add files

---
+++ message.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+info=$(last -F | grep -m 2 -v 'reboot' | awk 'NR==2 {print $1, $4, $5, $6, $7}')
+
+# Mostrar un cuadro de diálogo con el mensaje
+zenity --question --width=400 --text="Al ingresar a este dispositivo aceptas las politicas. La información que contiene este dispositivo es privada y de caracter confidencial. Ultimo inicio de sesión:\n$info"
